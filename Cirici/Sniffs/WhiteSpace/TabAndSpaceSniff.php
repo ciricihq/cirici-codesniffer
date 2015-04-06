aa95d70f7ff56d82c226288a0d718245f0b64f1b
Òscar Casajuana <elboletaire@underave.net>
2015-04-06 14:41:17 +0200

    We don't care about double spaces

--- Cirici/Sniffs/WhiteSpace/TabAndSpaceSniff.php
+++ Cirici/Sniffs/WhiteSpace/TabAndSpaceSniff.php
@@ -61,10 +61,6 @@ class Cirici_Sniffs_WhiteSpace_TabAndSpaceSniff implements PHP_CodeSniffer_Sniff
             return;
         }
 
-        if (strpos($tokens[$stackPtr]['content'], '  ') !== false) {
-            $error = 'Double space found';
-            $phpcsFile->addError($error, $stackPtr);
-        }
         if (strpos($tokens[$stackPtr]['content'], " \t") !== false) {
             $error = 'Space and tab found';
             $phpcsFile->addError($error, $stackPtr);
